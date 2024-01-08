3673f5315554756a6595637171a73fe39f0bb596
VISHNU PREMIL <82073583+strangedreamer4@users.noreply.github.com>
2024-01-08 04:35:00 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const video = document.getElementById('video');
+    const canvas = document.getElementById('canvas');
+    const displaySize = { width: video.width, height: video.height };
+    
+    await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+    await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+    await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+    
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    );
+    
+    video.addEventListener('play', () => {
+        const canvasDisplaySize = { width: video.width, height: video.height };
+        faceapi.matchDimensions(canvas, canvasDisplaySize);
+        
+        setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(video,
+                new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors().withFaceExpressions();
+            
+            const resizedDetections = faceapi.resizeResults(detections, canvasDisplaySize);
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+            faceapi.draw.drawDetections(canvas, resizedDetections);
+            faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+            faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+        }, 100);
+    });
+});
